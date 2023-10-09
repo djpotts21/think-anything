09aca63ade3c760062b48f5babf5470ec08407c5
Daniel Potts. <djpotts2891@gmail.com>
2023-10-09 22:20:17 +0000

    Added Reviews and Goals to Home

--- start.py
+++ start.py
@@ -9,6 +9,7 @@ from flask import (
     url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from datetime import date, datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
@@ -21,11 +22,41 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+today = date.today().strftime("%d %B, %Y")
+tommorow = datetime.now() + timedelta(1)
+tommorow_formated = tommorow.strftime("%d %B, %Y")
+
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+
+    reviews = list(mongo.db.reviews.find().sort("_id", -1).limit(3))
+    # goals = list(mongo.db.goals.find(
+    #     {"created_by": session["user"]},
+    #     {"date": today.strftime("%d %B, %Y")}).limit(3))
+    goals = list(mongo.db.goals.find(
+        {"created_by": "daniel", "date": today}).limit(3))
+    return render_template(
+        "home.html",
+        reviews=reviews,
+        goals=goals, tommorow=tommorow_formated)
+
+
+@app.route("/goal-done/<goal_id>", methods=["POST"])
+def goal_done(goal_id):
+    mongo.db.tasks.update_one(
+        {"_id": ObjectId(goal_id)},
+        {'$set': {"status": "done"}})
+    flash("Goal marked as done")
+
+
+@app.route("/goal-move-tomorrow/<goal_id>", methods=["POST"])
+def goal_move_tomorrow(goal_id):
+    mongo.db.tasks.update_one(
+        {"_id": ObjectId(goal_id)},
+        {'$set': {"date": tommorow_formated}})
+    flash("Goal moved to tommorow")
 
 
 if __name__ == "__main__":
@@ -33,4 +64,4 @@ if __name__ == "__main__":
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=True
-        )
+    )
