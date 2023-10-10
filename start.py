c7a3ce0271ade4284483afc40e381cbce6a1565e
Daniel Potts. <djpotts2891@gmail.com>
2023-10-10 23:36:51 +0000

    Style flash messages + Logout func

--- start.py
+++ start.py
@@ -75,7 +75,7 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                flash("Hi, {}".format(existing_user["full_name"].title()))
                 # return redirect(url_for("profile", username=session["user"]))
                 return redirect(url_for("home"))
             else:
@@ -88,6 +88,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html", background=background)
+
+
+@app.route("/logout")
+def logout():
+    # remove session cookie for user
+    flash("You have been logged out!")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 @app.route("/share-your-art", methods=["GET", "POST"])
