6abc62c181ecceb29a60960b02c25014162bdbf9
Daniel Potts. <djpotts2891@gmail.com>
2023-10-10 21:13:26 +0000

    Create login page and function

--- start.py
+++ start.py
@@ -61,6 +61,33 @@ def goal_move_tomorrow(goal_id):
     return redirect(url_for("home"))
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists #
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # checks if hashed password matches #
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                # return redirect(url_for("profile", username=session["user"]))
+                return redirect(url_for("home"))
+            else:
+                # invalid password #
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist #
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
